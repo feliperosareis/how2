6f5cf17aa79dba3445d0e5041b410549d2efb426
Felipe Rosa Reis <felipe.reis@leadforce.com.br>
2020-07-02 22:51:42 -0300

    -Alteração dos textos de marcação.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	
+	// Implementa o carrossel.
 	$('.jobs').owlCarousel({
 		loop: true,
 		autoPlay: true,
@@ -20,11 +21,12 @@ $(document).ready(function() {
 
 });
 
-
+// Rola a página até o topo.
 $('.go-top').click(function(){
 	$("html, body").animate({scrollTop: 0}, 1000);
 });
 
+// Adiciona/remove a classe que deixa o menu fixo.
 $(document).on('scroll', function(){
 	if ( $(window).scrollTop() > 10 )
 	{
@@ -46,6 +48,7 @@ $(document).on('scroll', function(){
 });
 
 
+// Rola a página conforme o menu clicado.
 $( "a.nav-link" ).click(function( event ) {
     event.preventDefault();
     $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
