66e50234fb3d70f0399884741ee0cea36ca7f555
Felipe Rosa Reis <felipe.reis@leadforce.com.br>
2020-07-02 21:34:14 -0300

    -Implementação do carrossel; -Menu fixo; -Redes sociais; -Botão subir para o topo;

---
+++ js/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+	
+	$('.jobs').owlCarousel({
+		loop: true,
+		autoPlay: true,
+		margin: 30,
+		dots: true,
+		nav: true,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			768: {
+				items: 3,
+				navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+			}
+		}
+	});
+
+
+});
+
+
+$('.go-top').click(function(){
+	$("html, body").animate({scrollTop: 0}, 1000);
+});
+
+$(document).on('scroll', function(){
+	if ( $(window).scrollTop() > 10 )
+	{
+		$('.navbar').addClass('fixed');
+	}
+	else
+	{
+		$('.navbar').removeClass('fixed');
+	}
+
+	if ( $(window).scrollTop() > 50 )
+	{
+		$('.go-top').show();
+	}
+	else
+	{
+		$('.go-top').hide();
+	}
+});
+
+
+$( "a.nav-link" ).click(function( event ) {
+    event.preventDefault();
+    $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
+});
